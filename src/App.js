2721c5734955d96245bcfbc643be813bab570fb5
mykenzie <mykenzierogers@gmail.com>
2019-05-22 20:35:50 -0500

    remove comment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import List from './List';
 import Checked from './Checked';
 import Unchecked from './Unchecked';
-//test
 function App() {
     return (
         <div>
